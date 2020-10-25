9aa36537e415bdd418db15b6faaa9c40255a9201
Jess Pring <jessicapring@outlook.com>
2020-10-25 22:55:03 +0000

    Index & show views

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,24 @@
 require 'open-uri'
 require 'nokogiri'
 
+puts "Deleting all plants..."
+Plant.destroy_all
+
+puts "Creating all plants..."
+
 url = 'https://www.houseofplants.co.uk/indexofplants.php'
 html_file = open(url).read
 html_doc = Nokogiri::HTML(html_file)
 
-html_doc.search('.product-inner-box').first(10).each do |element|
+html_doc.search('.product-inner-box').first(20).each do |element|
   latin = element.search('.latinName').text.strip
   name = element.search('.commonName').text.strip
   description = element.search('.description').text.strip
+  
+  img = element.css('img').attr('src').value
+  img_url = "https://www.houseofplants.co.uk/#{img}"
 
-  Plant.create(name: name, latin: latin, description: description)
+  Plant.create(name: name, latin: latin, description: description, image_url: img_url)
 end
+
+puts "Finished!"
