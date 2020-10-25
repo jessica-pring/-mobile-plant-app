6846c6326796c72a88cdf756e3dcb18bb8ddfb94
Jess Pring <jessicapring@outlook.com>
2020-10-25 23:19:51 +0000

    JS nav active class

---
+++ app/javascript/components/nav_active.js
@@ -0,0 +1,25 @@
+const navActive = () => {
+  const navContainer = document.querySelector('.navbar-collapse');
+  const btns = navContainer.querySelector('.nav-item');
+
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", () => {
+      const current = document.querySelector('.active');
+      console.log(current)
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+}
+
+export default navActive;
+
+// put your mapping here
+// var mapping = {
+//   "http://localhost:3000/": "css.path.to.your.element",
+//   "http://localhost:3000/plants": "path.to.other.element",
+// }
+
+// $(function () {
+//   $(mapping[window.location.href]).addClass('active');
+// });
